30ac2055c46fbf2eb0c873794598e67919ffae40
SerhiiYaroshenko1986 <datextdk@gmail.com>
2026-03-12 21:14:41 +0200

    refactor footer and hero components for improved design and navigation

--- src/app/components/footer-component/footer.component.ts
+++ src/app/components/footer-component/footer.component.ts
@@ -1,11 +1,12 @@
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
